3d34a3873c20292520c844049bf463053205c355
abbossth <a@test.ru>
2026-02-10 17:32:54 +0500

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 function Home() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <div className="home-container">
         <h1>Home</h1>
-        {/* rasm */}
-        {/* matn html har xil */}
+        <h3>Count</h3>
+        <p> {count} </p>
+        <div>
+          <button onClick={() => setCount(count + 1)} className="increment">
+            +1
+          </button>
+          <button className="decrement"> -1 </button>
+        </div>
       </div>
     </>
   );
